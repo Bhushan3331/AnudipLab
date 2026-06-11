b0d7c15a42e5fa2c094b8bb81d26edfb7cd3657a
Bhushan3331 <shashibhushan22102004@gmail.com>
2026-06-11 14:44:38 +0530

    Add files via upload

---
+++ Lab3.sql
@@ -0,0 +1,52 @@
+
+-- Task: Let's consider a scenario where you want to retrieve information about students
+
+-- from a database table named student and display the results in ascending order based on their last names.
+
+-- Student table contain column (stud_id,FirstName,LastName,Age,Phoneno,Address) add 5 records in tables.
+
+-- Hint: Use orderBy clause in a ascending Order
+
+
+mysql> create table students(stud_id varchar(10) not null,FirstName varchar(30),LastName varchar(30) not null,Age int not null,phoneno varchar(11) not null,Address varchar(40) not null);
+Query OK, 0 rows affected (0.04 sec)
+
+mysql> insert into students values(1,'Rohan','Jadhav',16,9894784894,'Thane'),(2,'Mohan','Singh',16,9876543211,'Airoli'),(3,'Riya','Bansal',16,8867452345,'Ghatkopar'),(4,'Suresh','Pal',16,6787564534,'Dadar'),(5,'Mukesh','koli',16,6874556787,'Diva');
+Query OK, 5 rows affected (0.01 sec)
+Records: 5  Duplicates: 0  Warnings: 0
+
+mysql> select * from students;
++---------+-----------+----------+-----+------------+-----------+
+| stud_id | FirstName | LastName | Age | phoneno    | Address   |
++---------+-----------+----------+-----+------------+-----------+
+| 1       | Rohan     | Jadhav   |  16 | 9894784894 | Thane     |
+| 2       | Mohan     | Singh    |  16 | 9876543211 | Airoli    |
+| 3       | Riya      | Bansal   |  16 | 8867452345 | Ghatkopar |
+| 4       | Suresh    | Pal      |  16 | 6787564534 | Dadar     |
+| 5       | Mukesh    | koli     |  16 | 6874556787 | Diva      |
++---------+-----------+----------+-----+------------+-----------+
+5 rows in set (0.00 sec)
+
+mysql> select * from students order by LastName;
++---------+-----------+----------+-----+------------+-----------+
+| stud_id | FirstName | LastName | Age | phoneno    | Address   |
++---------+-----------+----------+-----+------------+-----------+
+| 3       | Riya      | Bansal   |  16 | 8867452345 | Ghatkopar |
+| 1       | Rohan     | Jadhav   |  16 | 9894784894 | Thane     |
+| 5       | Mukesh    | koli     |  16 | 6874556787 | Diva      |
+| 4       | Suresh    | Pal      |  16 | 6787564534 | Dadar     |
+| 2       | Mohan     | Singh    |  16 | 9876543211 | Airoli    |
++---------+-----------+----------+-----+------------+-----------+
+5 rows in set (0.00 sec)
+
+mysql> select * from students order by LastName asc;
++---------+-----------+----------+-----+------------+-----------+
+| stud_id | FirstName | LastName | Age | phoneno    | Address   |
++---------+-----------+----------+-----+------------+-----------+
+| 3       | Riya      | Bansal   |  16 | 8867452345 | Ghatkopar |
+| 1       | Rohan     | Jadhav   |  16 | 9894784894 | Thane     |
+| 5       | Mukesh    | koli     |  16 | 6874556787 | Diva      |
+| 4       | Suresh    | Pal      |  16 | 6787564534 | Dadar     |
+| 2       | Mohan     | Singh    |  16 | 9876543211 | Airoli    |
++---------+-----------+----------+-----+------------+-----------+
+5 rows in set (0.00 sec)
